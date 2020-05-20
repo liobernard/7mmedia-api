ea017c6cf46d603bf2b14010c33d2f0e4889ef95
liobernard <lio@liobernard.com>
2020-05-20 02:15:49 -0400

    Reconfigures S3 API for filetype validation.

--- s3/api.py
+++ s3/api.py
@@ -1,125 +1,118 @@
 import logging
+import magic
+import requests
 from decouple import config
-from rest_framework.generics import GenericAPIView
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, ParseError, ValidationError
 
 import boto3
 from botocore.exceptions import ClientError
 from botocore.config import Config
-
-from .serializers import SignedUrlSerializer
 
 
 AWS_ACCESS_KEY_ID       = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY   = config('AWS_SECRET_ACCESS_KEY')
 AWS_S3_REGION_NAME      = config('AWS_S3_REGION_NAME')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_CDN_DOMAIN          = config('AWS_CDN_DOMAIN')
 
 
-class SignedUrlAPI(GenericAPIView):
+class S3API(APIView):
     permission_classes = [IsAdminUser]
-    serializer_class = SignedUrlSerializer
 
-    def create_presigned_post(self, bucket_name, object_name,
-                              fields=None, conditions=None, expiration=3600):
-        s3_client = boto3.client(
+    def get_s3_client(self):
+        return boto3.client(
             's3',
             aws_access_key_id=AWS_ACCESS_KEY_ID,
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-            config=Config(signature_version='s3v4', region_name=AWS_S3_REGION_NAME)
+            config=Config(
+                signature_version='s3v4', region_name=AWS_S3_REGION_NAME
+            )
         )
+
+    def list_objects(self):
+        s3_client = self.get_s3_client()
+
+        try:
+            response = s3_client.list_objects_v2(
+                Bucket=AWS_STORAGE_BUCKET_NAME,
+                Prefix=self.prefix
+            )['Contents']
+        except KeyError as e:
+            raise NotFound(detail='{} not found.'.format(e))
+        except ClientError as e:
+            raise NotFound(detail='Error: {}'.format(e))
+
+        return list(map(
+            lambda obj: 'https://' + AWS_CDN_DOMAIN + '/' + obj['Key'],
+            filter(
+                lambda obj: obj['Key'] is not self.prefix,
+                filter(lambda obj: obj['Size'] > 0, response)
+            )
+        ))
+
+    def validate_file(self, file):
+        magic_type = magic.from_buffer(file.read(2048), mime=True)
+        file.seek(0)
+
+        if magic_type != self.content_type:
+            main, sub = self.content_type.split('/', 1)
+            if (main != magic_type.split('/')[0] and sub != '*'):
+                raise ValidationError(
+                    'Error: Only {t} types supported. You '
+                    'submitted {s}.'.format(t=self.content_type, s=magic_type)
+                )
+        return file
+
+    def create_presigned_post(self, object_name):
+        s3_client = self.get_s3_client()
+
         try:
             response = s3_client.generate_presigned_post(
-                bucket_name, object_name,
-                Fields=fields, Conditions=conditions, ExpiresIn=expiration)
+                AWS_STORAGE_BUCKET_NAME,
+                object_name,
+                Fields={'acl': 'public-read'},
+                Conditions=[{'acl': 'public-read'}],
+                ExpiresIn=3600
+            )
         except ClientError as e:
             logging.error(e)
             return None
         return response
 
+    def get(self, request, *args, **kwargs):
+        objects = self.list_objects()
+        return Response(objects)
+
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        bucket_name = serializer.validated_data['bucket_name']
-        object_name = serializer.validated_data['object_name']
-        fields      = serializer.validated_data['fields']
-        conditions  = serializer.validated_data['conditions']
-        expiration  = serializer.validated_data['expiration']
+        if 'file' not in request.data:
+            raise ParseError('Empty content.')
 
-        response = self.create_presigned_post(
-            bucket_name, object_name,
-            fields, conditions, expiration
-        )
+        unvalidated = request.data['file']
+        file        = self.validate_file(unvalidated)
+        object_name = self.prefix + file.name
+        response    = self.create_presigned_post(object_name)
 
-        return Response({ 'url': response['url'], 'fields': response['fields'] })
+        return Response({'url': response['url'], 'fields': response['fields']})
 
 
-class ListThumbnailsAPI(APIView):
-    permission_classes = [IsAdminUser]
-
-    def list_thumbnails(self):
-        s3_client = boto3.client(
-            's3',
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-            config=Config(signature_version='s3v4', region_name=AWS_S3_REGION_NAME)
-        )
-
-        try:
-            response = s3_client.list_objects_v2(
-                Bucket=AWS_STORAGE_BUCKET_NAME,
-                Prefix='media/images/film_thumbnails/'
-            )['Contents']
-        except KeyError as e:
-            raise NotFound(detail='{e} not found.'.format(e=e))
-        except ClientError as e:
-            raise NotFound(detail='Error: {e}'.format(e=e))
-
-        return list(map(
-            lambda obj:
-                'https://assets.7mmedia.online/{key}'.format(key=obj['Key']),
-            filter(
-                lambda obj: obj['Key'] is not 'media/images/film_thumbnails/',
-                filter(lambda obj: obj['Size'] > 0, response)
-            )
-        ))
-
-    def get(self, request, *args, **kwargs):
-        return Response(self.list_thumbnails())
+class S3VideosAPI(S3API):
+    prefix          = 'media/videos/'
+    content_type    = 'video/mp4'
 
 
-class ListFilmsAPI(APIView):
-    permission_classes = [IsAdminUser]
+class S3FilmsAPI(S3API):
+    prefix          = 'media/videos/films/'
+    content_type    = 'video/mp4'
 
-    def list_films(self):
-        s3_client = boto3.client(
-            's3',
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-            config=Config(signature_version='s3v4', region_name=AWS_S3_REGION_NAME)
-        )
 
-        try:
-            response = s3_client.list_objects_v2(
-                Bucket=AWS_STORAGE_BUCKET_NAME,
-                Prefix='media/videos/films/'
-            )['Contents']
-        except KeyError as e:
-            raise NotFound(detail='{e} not found.'.format(e=e))
-        except ClientError as e:
-            raise NotFound(detail='Error: {e}'.format(e=e))
+class S3ImagesAPI(S3API):
+    prefix          = 'media/images/'
+    content_type    = 'image/*'
 
-        return list(map(
-            lambda obj:
-                'https://assets.7mmedia.online/{key}'.format(key=obj['Key']),
-            filter(
-                lambda obj: obj['Key'] is not 'media/videos/films/',
-                filter(lambda obj: obj['Size'] > 0, response)
-            )
-        ))
 
-    def get(self, request, *args, **kwargs):
-        return Response(self.list_films())
+class S3FilmThumbnailsAPI(S3API):
+    prefix          = 'media/images/film_thumbnails/'
+    content_type    = 'image/*'
--- s3/endpoints.py
+++ s3/endpoints.py
@@ -1,9 +1,10 @@
 from django.urls import re_path
 
-from .api import ListFilmsAPI, ListThumbnailsAPI, SignedUrlAPI
+from .api import S3VideosAPI, S3FilmsAPI, S3ImagesAPI, S3FilmThumbnailsAPI
 
 urlpatterns = [
-    re_path(r'^api/s3/list_objects/video/$', ListFilmsAPI.as_view()),
-    re_path(r'^api/s3/list_objects/thumbnail/$', ListThumbnailsAPI.as_view()),
-    re_path(r'^api/s3/signed_url/$', SignedUrlAPI.as_view()),
+    re_path(r'^api/s3/media/videos/$', S3VideosAPI.as_view()),
+    re_path(r'^api/s3/media/videos/films/$', S3FilmsAPI.as_view()),
+    re_path(r'^api/s3/media/images/$', S3ImagesAPI.as_view()),
+    re_path(r'^api/s3/media/images/film_thumbnails/$', S3FilmThumbnailsAPI.as_view()),
 ]
